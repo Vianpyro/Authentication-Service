2a7be7bc28480917d3b8b0d0b9d25b0fdf5b4442
Vianpyro <vianney@veremme.org>
2025-07-09 18:35:29 +0000

    Update email confirmation route prefix for consistency in authentication endpoints

--- api/app/routes/v1/endpoints/authentication/__init__.py
+++ api/app/routes/v1/endpoints/authentication/__init__.py
@@ -6,6 +6,6 @@ from .register import router as register_router
 
 router = APIRouter()
 
-router.include_router(email_confirm_router, prefix="/confirm", tags=["Authentication", "Email Confirmation"])
+router.include_router(email_confirm_router, prefix="/register/confirm", tags=["Authentication", "Registration"])
 router.include_router(login_router, prefix="/login", tags=["Authentication", "Login"])
 router.include_router(register_router, prefix="/register", tags=["Authentication", "Registration"])
