5796d9176c9f929bbffde8bafedff91d12936b97
Vianpyro <vianney@veremme.org>
2025-07-13 19:27:47 +0000

    Refactor application routes to use get_application_controller for dependency injection

--- api/app/v1/routes/application.py
+++ api/app/v1/routes/application.py
@@ -1,10 +1,9 @@
 from uuid import UUID
 
-from app.utility.database import get_db
-from fastapi import APIRouter, Depends
-from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import APIRouter, Depends, status
 
-from ..controllers.application import ApplicationController
+from ..controllers import get_application_controller as get_app_controller
+from ..controllers.application import ApplicationController as AppController
 from ..schemas.application import (
     AppCreate,
     AppCreateResponse,
@@ -18,24 +17,20 @@ router = APIRouter()
 
 
 @router.get("/{app_id}", response_model=AppGetResponse)
-async def get_applications(app_id: UUID, db: AsyncSession = Depends(get_db)):
-    controller = ApplicationController(db)
+async def get_applications(app_id: UUID, controller: AppController = Depends(get_app_controller)):
     return await controller.get_application_by_id(app_id)
 
 
-@router.post("/register", response_model=AppCreateResponse, status_code=201)
-async def register_application(payload: AppCreate, db: AsyncSession = Depends(get_db)):
-    controller = ApplicationController(db)
+@router.post("/register", response_model=AppCreateResponse, status_code=status.HTTP_201_CREATED)
+async def register_application(payload: AppCreate, controller: AppController = Depends(get_app_controller)):
     return await controller.create_application(payload)
 
 
 @router.patch("", response_model=AppUpdateResponse)
-async def update_application(payload: AppUpdate, db: AsyncSession = Depends(get_db)):
-    controller = ApplicationController(db)
+async def update_application(payload: AppUpdate, controller: AppController = Depends(get_app_controller)):
     return await controller.update_application(payload)
 
 
 @router.delete("/{app_id}", response_model=AppDeleteResponse)
-async def delete_application(app_id: UUID, db: AsyncSession = Depends(get_db)):
-    controller = ApplicationController(db)
+async def delete_application(app_id: UUID, controller: AppController = Depends(get_app_controller)):
     return await controller.delete_application(app_id)
